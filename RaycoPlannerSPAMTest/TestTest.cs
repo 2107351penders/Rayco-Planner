1bcebc9a758fa7e3c3270ef00df1356b2c717add
Tim Penders <2107351penders@zuyd.nl>
2023-01-02 19:19:39 +0100

    Restructure project to include NUnit tests

---
+++ RaycoPlannerSPAMTest/TestTest.cs
@@ -0,0 +1,22 @@
+using RaycoPlannerSPAM;
+
+namespace RaycoPlannerSPAMTest
+{
+    public class Tests
+    {
+        private ProjectSoort projectSoort;
+        private string projectSoortNaam = "Test project";
+        [SetUp]
+        public void Setup()
+        {
+            projectSoort = new ProjectSoort(projectSoortNaam);
+        }
+
+        [Test]
+        public void TestTest()
+        {
+            // Test of ons test project naar behoren werkt en objecten kan maken uit het app project
+            Assert.AreEqual(projectSoort.naam, projectSoortNaam);
+        }
+    }
+}
